616b4614a92c5f62d87ef31a25f2b4a99cfa734d
Jasurbek Shomaqsudov <107366287+Jasurbek2208@users.noreply.github.com>
2023-05-02 16:47:20 +0500

    feat: add animattion for car model

--- src/App.js
+++ src/App.js
@@ -15,11 +15,32 @@ function Model({ url, initialPosition }) {
     });
   }, [url]);
 
+  function reset() {
+    setPosition({ x: -6, y: 0, z: 0 })
+  }
+
+  function animatedCar() {
+    if (position.x === -35) {
+      reset()
+      return;
+    }
+
+    setTimeout(() => {
+      setPosition(p => ({ ...p, z: position.z - 5, x: position.x - 1, y: position.y - 0.01 }))
+    }, 80);
+  }
+
+  useEffect(() => {
+    animatedCar()
+  }, [position])
+
+
   useFrame(() => {
     if (groupRef.current) {
       groupRef.current.position.x = position.x;
       groupRef.current.position.y = position.y;
       groupRef.current.position.z = position.z;
+      groupRef.current.rotation.y = -Math.PI;
     }
   });
 
@@ -40,7 +61,7 @@ function App() {
       <Canvas>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <Model url="3D-models/scene.gltf" initialPosition={{ x: 0, y: 0, z: 0 }} />
+        <Model url="3D-models/scene.gltf" initialPosition={{ x: -6, y: 0, z: 0 }} />
         <OrbitControls />
       </Canvas>
     </div>
